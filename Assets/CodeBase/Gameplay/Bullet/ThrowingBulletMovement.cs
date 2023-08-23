232646d5192c3f5f44f18c2880c7f56fba726e79
SHIPZz <mrartem444russ@gmail.com>
2023-08-23 04:14:20 +0300

    refactor throwingmovement

--- Assets/CodeBase/Gameplay/Bullet/ThrowingBulletMovement.cs
+++ Assets/CodeBase/Gameplay/Bullet/ThrowingBulletMovement.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using CodeBase.Gameplay.Collision;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 namespace CodeBase.Gameplay.Bullet
@@ -7,62 +11,87 @@ namespace CodeBase.Gameplay.Bullet
     public class ThrowingBulletMovement : BulletMovement
     {
         [SerializeField] private Vector3 _bulletModelRotation;
-        
+        private TriggerObserver _triggerObserver;
+
         private ThrowingBullet _throwingBullet;
-
-        private Vector3 _targetPosition;
-        private Vector3 _startPosition;
-        private Vector3 _moveDirection;
-
-        private bool _isMoving;
+        private bool _isBlocked;
+        private Vector3 _currentVelocity;
+        private Coroutine _moveCoroutine;
+        private Tweener _tweener;
+        private Vector3 _angularVelocity;
 
         protected override void Awake()
         {
             base.Awake();
             _throwingBullet = GetComponent<ThrowingBullet>();
             RigidBody.isKinematic = false;
+            _triggerObserver = GetComponent<TriggerObserver>();
+            _triggerObserver.Entered += OnTriggerObserverOnEntered;
         }
 
-        private void FixedUpdate()
+        private void OnTriggerObserverOnEntered(Collider a)
         {
-            if (!_isMoving)
-                return;
-            
-            Vector3 velocity = _moveDirection.normalized * 30;
-            RigidBody.velocity = velocity;
-        }
-
-        private void OnDisable()
-        {
-            _isMoving = false;
+            _isBlocked = true;
         }
 
         public override void Move(Vector3 target, Vector3 startPosition)
         {
-            _targetPosition = target;
-            _startPosition = startPosition;
-            _moveDirection = _targetPosition - _startPosition;
-            
-            transform.forward = target;
-            transform.position = startPosition;
-            
+            var moveDirection = target - startPosition;
+
+            SetMove(moveDirection, target, startPosition);
             Rotate(_throwingBullet, RotateDuration);
-            
-            _isMoving = true;
+        }
+
+        private void SetMove(Vector3 moveDirection, Vector3 target, Vector3 startPosition)
+        {
+            transform.forward = moveDirection;
+            transform.position = startPosition;
+
+            if (_moveCoroutine != null)
+                StopCoroutine(_moveCoroutine);
+
+            _moveCoroutine = StartCoroutine(StartMoveCoroutine(moveDirection, 25));
+        }
+
+        private IEnumerator StartMoveCoroutine(Vector3 target, float speed)
+        {
+            while (Vector3.Distance(transform.position, target) > 0.1)
+            {
+                if (_isBlocked)
+                {
+                    RigidBody.angularVelocity = Vector3.zero;
+                    RigidBody.isKinematic = true;
+                    transform.forward = target;
+                    Rotate(_throwingBullet);
+                    break;
+                }
+
+                _currentVelocity = Vector3.Lerp(_currentVelocity, target.normalized * speed, Time.deltaTime * 20);
+                RigidBody.velocity = _currentVelocity;
+                _angularVelocity = Vector3.Lerp(_angularVelocity, transform.right * 80, Time.deltaTime * 80);
+                RigidBody.angularVelocity = _angularVelocity;
+                // RigidBody.AddTorque(_angularVelocity);
+
+                yield return new WaitForFixedUpdate();
+            }
         }
 
         private void Rotate(ThrowingBullet throwingBullet, float rotateDuration)
         {
-            Vector3 startTargetRotation = new Vector3(104, transform.localEulerAngles.y,
-                transform.localEulerAngles.z);
+            Rotate(throwingBullet);
 
-            transform.DORotate(startTargetRotation, 0f);
+            // Vector3 flyRotation = new Vector3(360, 0, 0);
+            // _tweener = RigidBody.DORotate(flyRotation, rotateDuration).SetRelative(true).SetEase(Ease.Linear);
+        }
 
-            Vector3 flyRotation = new Vector3(360, 0, 0);
+        private void Rotate(ThrowingBullet throwingBullet)
+        {
+            Vector3 startTargetRotation = new Vector3(104, transform.eulerAngles.y,
+                transform.eulerAngles.z);
+
+            RigidBody.DORotate(startTargetRotation, 0f);
 
             throwingBullet.BulletModel.DOLocalRotate(_bulletModelRotation, 0);
-
-            // transform.DORotate(flyRotation, rotateDuration).SetRelative(true).SetEase(Ease.Linear);
         }
     }
 }
