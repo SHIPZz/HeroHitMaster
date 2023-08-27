6dcd7600320d2749d86f16f2c94e9d5b8e9dd845
SHIPZz <mrartem444russ@gmail.com>
2023-08-27 16:23:05 +0300

    update popupinfoview

--- Assets/CodeBase/UI/Windows/Popup/PopupInfoView.cs
+++ Assets/CodeBase/UI/Windows/Popup/PopupInfoView.cs
@@ -1,22 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CodeBase.Enums;
 using CodeBase.Services.Providers;
 using CodeBase.UI.Weapons;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 using Random = UnityEngine.Random;
 
 namespace CodeBase.UI.Windows.Popup
 {
-    public class PopupInfoView : MonoBehaviour
+    public class PopupInfoView : SerializedMonoBehaviour
     {
+        [OdinSerialize] private Dictionary<WeaponTypeId, Image> _whiteFrames;
         private Dictionary<WeaponTypeId, WeaponSelectorView> _weaponIcons;
+
+        private float _nextWeaponSwitchTime = 1f;
 
         [Inject]
         private void Construct(IProvider<WeaponIconsProvider> provider)
         {
             _weaponIcons = provider.Get().PopupIcons;
         }
+
+        private void Awake()
+        {
+            foreach (var whiteFrame in _whiteFrames.Values)
+            {
+                Color currentColor = whiteFrame.color;
+                
+                Color newColor = new Color(currentColor.r, currentColor.g, currentColor.b, 0f);
+                
+                whiteFrame.color = newColor;
+            }
+        }
+
+        // public WeaponTypeId GetRandomTargetWeapon()
+        // {
+        //     return WeaponTypeId.BlueWeapon;
+        // }
 
         private void OnEnable()
         {
