0bb5b7a3a8eec045efbaeb6b5e15f55f6ac1ed6a
SHIPZz <mrartem444russ@gmail.com>
2023-08-30 03:07:20 +0300

    Update DestroyableObjectBodyPart.cs

--- Assets/CodeBase/Gameplay/ObjectBodyPart/DestroyableObjectBodyPart.cs
+++ Assets/CodeBase/Gameplay/ObjectBodyPart/DestroyableObjectBodyPart.cs
@@ -1,34 +1,40 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace CodeBase.Gameplay.ObjectBodyPart
 {
     public class DestroyableObjectBodyPart : MonoBehaviour
     {
-        private const float PositionY = 1;
         private const float Force = 200f;
-        private const float RandomValueForVector = 1f;
-        
+        private const float AutoDestroyDelay = 10;
+
         [SerializeField] private List<Rigidbody> _parts;
-        [field: SerializeField] public DestroyableObjectTypeId DestroyableObjectTypeId { get; private set; }
         
+        [field: SerializeField] public DestroyableObjectTypeId DestroyableObjectTypeId { get; private set; }
+
         public void Enable()
         {
             _parts.ForEach(x =>
             {
                 x.isKinematic = false;
-                
+
                 Vector3 randomDirection = new Vector3(
-                    Random.Range(-1f, 1f), 
-                    Random.Range(-1f, 1f), 
+                    Random.Range(-1f, 1f),
+                    Random.Range(-1f, 1f),
                     Random.Range(-1f, 1f)
                 ).normalized;
 
                 x.AddForce(randomDirection * Force, ForceMode.Force);
             });
+
+            DOTween.Sequence().AppendInterval(AutoDestroyDelay).OnComplete(Disable);
         }
 
-        public void Disable() =>
+        private void Disable()
+        {
+            DOTween.Kill(transform);
             gameObject.SetActive(false);
+        }
     }
 }
