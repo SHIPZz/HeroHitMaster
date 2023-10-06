627f475c96de08ede774e566c28610e792348450
SHIPZz <mrartem444russ@gmail.com>
2023-10-06 03:10:04 +0300

    Update BlockScrollButtonsOnChange.cs

--- Assets/CodeBase/UI/ShopScrollRects/BlockScrollButtonsOnChange.cs
+++ Assets/CodeBase/UI/ShopScrollRects/BlockScrollButtonsOnChange.cs
@@ -8,7 +8,7 @@ namespace CodeBase.UI.ShopScrollRects
 {
     public class BlockScrollButtonsOnChange : IInitializable, IDisposable
     {
-        private ScrollRectChanger _scrollRectChanger;
+        private readonly ScrollRectChanger _scrollRectChanger;
         private readonly List<Button> _buttons = new();
 
         public BlockScrollButtonsOnChange(ScrollRectChanger scrollRectChanger, IProvider<List<ScrollRectView>> provider)
@@ -17,15 +17,11 @@ namespace CodeBase.UI.ShopScrollRects
             provider.Get().ForEach(x => _buttons.Add(x.Button));
         }
 
-        public void Initialize()
-        {
+        public void Initialize() => 
             _scrollRectChanger.Changed += SetActive;
-        }
 
-        public void Dispose()
-        {
-            _scrollRectChanger.Changed -= SetActive;
-        }
+        public void Dispose() => 
+        _scrollRectChanger.Changed -= SetActive;
 
         private void SetActive(bool isChanged)
         {
