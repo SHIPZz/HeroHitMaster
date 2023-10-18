20b524bfbd12f030e06c4630125c0b9dcf258b17
SHIPZz <mrartem444russ@gmail.com>
2023-10-18 23:45:38 +0300

    update china2 scene, init new scripts

---
+++ Assets/CodeBase/Gameplay/EnableUIOnPlayerAggroZoneEnter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CodeBase.Gameplay.Character.Players;
+using UnityEngine;
+
+namespace CodeBase.Gameplay
+{
+    [RequireComponent(typeof(AggroZone))]
+    public class EnableUIOnPlayerAggroZoneEnter : MonoBehaviour
+    {
+        [SerializeField] private Canvas _mainCanvas;
+
+        private AggroZone _aggroZone;
+
+        private void Awake()
+        {
+            _mainCanvas.gameObject.SetActive(false);
+            _aggroZone = GetComponent<AggroZone>();
+        }
+
+        private void OnEnable() => 
+            _aggroZone.PlayerEntered += Turn;
+
+        private void OnDisable() => 
+            _aggroZone.PlayerEntered -= Turn;
+
+        private void Turn(Player obj) => 
+            _mainCanvas.gameObject.SetActive(true);
+    }
+}
